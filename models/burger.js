1f7585ad06878c168e85b30c54ac40e0b59f06c0
Jerry Leong <jerry.leong20@gmail.com>
2020-10-23 21:54:47 -0700

    add controllers folder and burgers_controller.js and export router

--- models/burger.js
+++ models/burger.js
@@ -15,12 +15,7 @@ var burger = {
     orm.updateOne("burgers", objColVals, condition, function(res) {
       cb(res);
     });
-  },
-//   delete: function(objColVals, cb) {
-//     orm.delete("cburgers", objColVals, function(res){
-//       cb(res);
-//     });
-//   }
+  }
 };
 
 module.exports = burger;
